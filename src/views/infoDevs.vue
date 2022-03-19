17fec8a5ce29be32327f85652d75317147dafc4c
unknown <fernandoftasca@gmail.com>
2022-03-19 12:52:46 -0300

    paginaInfoDevs

---
+++ src/views/infoDevs.vue
@@ -0,0 +1,15 @@
+<template>
+  <infoDevsComp />
+</template>
+
+<script>
+  import infoDevsComp from '../components/infoDevsComp'
+
+  export default {
+    name: 'infoDevs',
+
+    components: {
+      infoDevsComp,
+    },
+  }
+</script>
