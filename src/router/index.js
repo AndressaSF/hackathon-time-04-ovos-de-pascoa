8520e0d09ca3fa0eea02af6cd16221d0213c8c7b
unknown <fernandoftasca@gmail.com>
2022-03-19 13:21:51 -0300

    index_info_devs

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import PaginaLogin from"../views/PaginaLogin.vue"
-=======
 import LojaView from '../views/LojaView.vue'
->>>>>>> f54519afeb8c98731b76e96ced721c30e6fb8008
+import infoDevs from '../views/infoDevs.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-<<<<<<< HEAD
     path: '/',
     name: 'home',
     component: HomeView
@@ -28,13 +25,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: AboutView
-=======
+    component: AboutView,
+  },
+  {
     path: '/loja',
     name: 'loja',
     component: LojaView
->>>>>>> f54519afeb8c98731b76e96ced721c30e6fb8008
+  },
+
+  {
+    path: '/informacoes-sobre-desenvolvedores',
+    name: 'infoDevs',
+    component: infoDevs
   }
+
 ]
 
 const router = new VueRouter({
