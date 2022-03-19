39418a9ecf330bf0c23fb30766b7edc1235947e4
luizhss <luizhsilvasantos@gmail.com>
2022-03-19 13:13:30 -0400

    extra: modo escuro

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,22 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
+import colors from "vuetify/lib/util/colors";
+
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: colors.purple.darken1,
+            secondary: colors.grey.darken1,
+            accent: colors.shades.black,
+            error: colors.red.accent3,
+            background: colors.grey.lighten5,
+          },
+          dark: {
+            primary: colors.purple.lighten1,
+            background: colors.indigo.base,
+          },
+        },
+      },
 });
--- src/shared/BarraMenu.vue
+++ src/shared/BarraMenu.vue
@@ -4,6 +4,26 @@
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-toolbar-title>NomeAPP</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-tooltip v-if="!$vuetify.theme.dark" top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+            <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+          </v-btn>
+        </template>
+        <span>Modo escuro desligado</span>
+      </v-tooltip>
+
+      <v-tooltip v-else top>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+            <v-icon>mdi-white-balance-sunny</v-icon>
+          </v-btn>
+        </template>
+        <span>Modo escuro ligado</span>
+      </v-tooltip>
     </v-app-bar>
 
     <v-navigation-drawer v-model="drawer" absolute temporary>
@@ -78,6 +98,11 @@ export default {
     drawer: false,
     group: null,
   }),
+  methods: {
+    darkMode() {
+        this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      }
+  }
 };
 </script>
 
