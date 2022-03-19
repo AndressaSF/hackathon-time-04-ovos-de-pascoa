a6fe97fc4de1a9985ca254e07e01d78bb7e0a84f
unknown <fernandoftasca@gmail.com>
2022-03-19 14:05:58 -0300

    infoDevs

--- src/components/infoDevsComp.vue
+++ src/components/infoDevsComp.vue
@@ -1,28 +1,36 @@
 <template>
     <div class="paginaDevs">
     <h2 class="tituloPagina">Desenvolvedores do Projeto</h2>
-    <ul class="listaDevs">
-        <li>
+    <h3>Conheça um pouco sobre os nossos desenvolvedores.</h3>
+    <div class="nomeDev">
+        <ul>
+        <ol>
             Andressa Freitas
-            <ol>Informações</ol>
-        </li>
-        <li>
+            <ol><a href="https://github.com/AndressaSF">Perfil do GitHub</a></ol>
+            <ol><a href="">Perfil do Linkedin</a></ol>
+        </ol>
+        <ol>
             Fernando Tasca
-            <ol>Informações</ol>
-        </li>
-        <li>
+            <ol><a href="https://github.com/FernandoTasca">Perfil do GitHub</a></ol>
+            <ol><a href="">Perfil do Linkedin</a></ol>
+        </ol>
+        <ol>
             Luiz Henrique
-            <ol>Informações</ol>
-        </li>
-        <li>
+            <ol><a href="https://github.com/luizhss">Perfil do GitHub</a></ol>
+            <ol><a href="">Perfil do Linkedin</a></ol>
+        </ol>
+        <ol>
             Tamara Luz
-            <ol>Informações</ol>
-        </li>
-        <li>
-            Wilseny
-            <ol>Informações</ol>
-        </li>
+            <ol><a href="https://github.com/tamarabluz">Perfil do GitHub</a></ol>
+            <ol><a href="">Perfil do Linkedin</a></ol>
+        </ol>
+        <ol>
+            Wilseny Teixeira
+            <ol><a href="https://github.com/WilsenyTeixeira">Perfil do GitHub</a></ol>
+            <ol><a href="">Perfil do Linkedin</a></ol>
+        </ol>
     </ul>
+    </div>
     </div>
         
 </template>
@@ -34,7 +42,25 @@ export default {
 </script>
 
 <style scoped>
-    .listaDevs{
+    *{
         align-content: center;
+        text-decoration: none;
     }
+
+    h2{
+        text-align: center  ;
+    }
+
+    h3{
+        text-align: center;
+    }
+
+    .nomeDev{
+        font-weight: 700;
+    }
+
+    a{
+        font-weight: 450;
+    }
+
 </style>
