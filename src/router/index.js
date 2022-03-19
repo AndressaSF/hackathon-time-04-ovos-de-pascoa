a2b63083e88b0db34b219b06e738d377f7afa7ae
tamara <tamarabluz@gmail.comgit initgit config --global user.email tamarabluz@gmail.com>
2022-03-19 12:42:44 -0300

    login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import PaginaLogin from"../views/PaginaLogin.vue"
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'PaginaLogin',
+    component: PaginaLogin
   },
   {
     path: '/about',
