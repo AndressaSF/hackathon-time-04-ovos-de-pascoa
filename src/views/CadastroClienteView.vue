df8d9dae5244f842bae888bdc7ad327c861ecd9b
AndressaSF <andressa.schf@hotmail.com>
2022-03-19 13:02:05 -0400

    Pagina CadastroCliente pronta

---
+++ src/views/CadastroClienteView.vue
@@ -0,0 +1,89 @@
+<template>
+<div style="width: 80%" class="registro">
+    <h2>Cadastro de Cliente</h2>
+    <v-form class="registro-box" ref="form" v-model="valid" lazy-validation>
+        <v-text-field v-model="nome" label="Nome Completo" required>
+        </v-text-field>
+        <v-text-field v-model="telefone" label="Telefone" required >
+        </v-text-field>
+        <v-text-field v-model="email" label="E-mail" required>
+        </v-text-field>
+        <v-text-field v-model="endereco" label="Endereço" required>
+        </v-text-field>
+        <v-text-field v-model="number" label="CPF" required>
+        </v-text-field>
+
+        <v-text-field v-model="password" type="password" label="Senha" required>
+        </v-text-field>
+
+    </v-form>
+        <div class="botoes">
+            <v-btn
+                class="mx-2"
+                color="purple"
+                elevation="2"
+                medium
+                outlined
+                rounded
+                @click="cadastrar"
+            >
+                Cadastrar
+            </v-btn>
+
+            <v-btn
+                class="mx-2"
+                color="purple"
+                elevation="2"
+                medium
+                outlined
+                rounded
+                @click="limpar"
+            >
+                Limpar
+            </v-btn>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "CadastroClienteView.vue",
+    data() {
+        return {
+            nomeVendedor: "",
+            nomeLoja: "",
+            cnpj: "",
+            endereco: "",
+            telefone: "",
+            email: "",
+            senha: "",
+        };
+    },
+
+    methods: {
+        cadastrar() {},
+        limpar() {
+            this.nomeVendedor = "";
+            this.nomeLoja = "";
+            this.cnpj = "";
+            this.endereco = "";
+            this.telefone = "";
+            this.email = "";
+            this.senha = "";
+        },
+    },
+};
+</script>
+
+<style>
+.registro {
+    text-align: center;
+    margin: auto;
+    margin-top: 30px;
+    
+}
+.registro-box {
+    max-width: 100vw;
+}
+</style>
