624e974ff2eca1fcb100e204e71ba4f27bae9df2
luizhss <luizhsilvasantos@gmail.com>
2022-03-19 11:23:08 -0400

    Loja adicionada

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import LojaView from '../views/LojaView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: AboutView
+    path: '/loja',
+    name: 'loja',
+    component: LojaView
   }
 ]
 
---
+++ src/views/LojaView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <v-container>
+      <CardOvosPascoa
+        v-for="ovoPascoa in ovosPascoa"
+        :key="ovoPascoa.id"
+        :nome="ovoPascoa.nome"
+        :preco="ovoPascoa.preco"
+        :imagem="ovoPascoa.imagem"
+        :sabor="ovoPascoa.sabor"
+        :endereco="ovoPascoa.local.endereco"
+        :cep="ovoPascoa.local.cep"
+        :nomeLocal="ovoPascoa.local.nome"
+      ></CardOvosPascoa>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import CardOvosPascoa from "../components/CardOvosPascoa";
+export default {
+  components: {
+    CardOvosPascoa,
+  },
+  methods: {},
+  data() {
+    return {
+      ovosPascoa: [],
+    };
+  },
+  created() {
+    fetch("https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json")
+      .then((response) => response.json())
+      .then((json) => (this.ovosPascoa = json));
+  },
+};
+</script>
+
+<style>
+</style>
