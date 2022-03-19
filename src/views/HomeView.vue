a82a06813e9bd5e3d3fc693bd128947455def144
WilsenyTeixeira <wilseny@outlook.com>
2022-03-19 16:24:15 -0300

    formatando_codigo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,85 +1,77 @@
 <template>
-  <v-container> 
+  <v-container>
+
     <router-link to="/login">
     <v-card
-    class="mx-auto"
-    max-width="344"
+      class="mx-auto"
+      max-width="344"
     >
-    <v-img
-      src="../assets/btn2.jpg"
-      height="40px"
-    ></v-img>
-
-  </v-card>
+      <v-img
+        src="../assets/btn2.jpg"
+        height="40px"
+      ></v-img>
+    </v-card>
     </router-link>
+
     <v-card
-    class="mx-auto first"
-    max-width="344"
+      class="mx-auto first"
+      max-width="344"
     >
-    <v-img
-      src="../assets/promocao.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      NÃO PERCA NOSSAS OFERTAS
-    </v-card-title>
-
-    <v-card-actions>
-      <router-link to="/loja">
-        <v-btn
-          color="primary"
-          text
-        >
-          Compre Aqui
-        </v-btn>
-      </router-link>
-      
-
-      <v-spacer></v-spacer>
-
-    </v-card-actions>
-  </v-card>
+      <v-img
+        src="../assets/promocao.jpg"
+        height="200px"
+      ></v-img>
+      <v-card-title>
+        NÃO PERCA NOSSAS OFERTAS
+      </v-card-title>
+      <v-card-actions>
+        <router-link to="/loja">
+          <v-btn
+            color="primary"
+            text
+          >
+            Compre Aqui
+          </v-btn>
+        </router-link>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
 
     <v-card
     class="mx-auto second"
     max-width="344"
-  >
-    <v-img
-      src="../assets/pascoamain.jpg"
-      height="200px"
-    ></v-img>
-
-    <v-card-title>
-      CADASTRE-SE 
-    </v-card-title>
-
-    <v-card-subtitle>
-      E RECEBA DIVERSOS BENEFÍCIOS
-    </v-card-subtitle>
-
-    <v-card-actions>
-      <router-link to="/cadastro-clientes">
-        <v-btn
-          color="primary"
-          text
-        >
-          Cliente
-        </v-btn>
-      </router-link>
-      <v-spacer></v-spacer>
-      <router-link to="/cadastro-vendedores">
-        <v-btn
-          color="primary"
-          text
-        >
-          Vendedor
-        </v-btn>
-      </router-link>
-    </v-card-actions>
-  </v-card>
+    >
+      <v-img
+        src="../assets/pascoamain.jpg"
+        height="200px"
+      ></v-img>
+      <v-card-title>
+        CADASTRE-SE 
+      </v-card-title>
+      <v-card-subtitle>
+        E RECEBA DIVERSOS BENEFÍCIOS
+      </v-card-subtitle>
+      <v-card-actions>
+        <router-link to="/cadastro-clientes">
+          <v-btn
+            color="primary"
+            text
+          >
+            Cliente
+          </v-btn>
+        </router-link>
+        <v-spacer></v-spacer>
+        <router-link to="/cadastro-vendedores">
+          <v-btn
+            color="primary"
+            text
+          >
+            Vendedor
+          </v-btn>
+        </router-link>
+      </v-card-actions>
+    </v-card>
   </v-container>
-  
 </template>
 
 <script>
