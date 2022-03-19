c4b32e8515ed67906c46cf1c45e17e3bad4ba5a3
AndressaSF <andressa.schf@hotmail.com>
2022-03-19 11:18:41 -0400

    BarraMenu base feita

---
+++ src/shared/BarraMenu.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="barra-menu">
+    <v-app-bar color="deep-purple" dark>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>NomeAPP</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>PaginaInicial</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/login">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/cadastro-clientes">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Cadastro Clientes</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/cadastro-vendedores">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Cadastro Vendedores</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/loja">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-cart</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Loja</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+          <router-link to="/informacoes-sobre-desenvolvedores">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-information-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Informações sobre os desenvolvedores</v-list-item-title>
+            </v-list-item>
+          </router-link>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+<script>
+export default {
+  name: "BarraMenu",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
+</script>
+
+<style scoped>
+
+</style>
